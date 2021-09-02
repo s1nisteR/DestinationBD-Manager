a2978d3e692bbe92729ca0eab67efedc4ca7a90f
s1nisteR <ankon1441@gmail.com>
2021-09-02 22:06:09 +0600

    Initial commit

---
+++ src/dbdm/Main.java
@@ -0,0 +1,16 @@
+package dbdm;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setSize(400, 500);
+
+        frame.setLayout(null);
+
+        frame.setVisible(true);
+    }
+}
